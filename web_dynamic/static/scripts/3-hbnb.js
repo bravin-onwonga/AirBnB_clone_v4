84e921503733f9472d4a1529df39cbb3af77090e
bravin-onwonga <onbravin@gmail.com>
2024-06-03 00:33:38 +0300

    Serve content from front-end

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -73,4 +73,35 @@ $(document).ready($(function () {
   }
 
   getStatus();
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: '{}',
+    contentType: 'application/json',
+    success: function (response) {
+      $.each(response, function (index, place) {
+        const placeElement = `
+        <article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest}Guest${place.max_guest !== 1 ? 's' : ''}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+          </div>
+          <div class="description">
+            ${place.description || 'safe'}
+          </div>
+        </article>`;
+
+        $('section.places').append(placeElement);
+      });
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log('Failure');
+    }
+  });
 }));
